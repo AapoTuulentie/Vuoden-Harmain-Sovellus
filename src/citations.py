583a775ee1974bef28a3af4e079549022b709b50
Ranta Eero O <eeranta@cubbli22-gold.local>
2022-12-09 14:05:49 +0200

    some refactoring + one robot test

--- src/citations.py
+++ src/citations.py
@@ -61,16 +61,18 @@ def form_citations_list():
         return False
     citations = get_citations()
     for citation in citations:
-        (citation_id, author, title, publisher,
-         year, doi, isbin, editor, pages, shorthand) = citation
-        section = [author, title, publisher, year, doi, isbin, editor, pages, shorthand]
+        (citation_id, author, title, publisher, year,
+        doi, isbin, editor, pages, shorthand, user_id, citationtype) = citation
+        section = [author, title, publisher, year, doi, isbin, editor, pages, shorthand, citationtype]
         citation_list.append((add_section_to_citation(section), citation_id))
     return citation_list
 
 def add_section_to_citation(section):
     citation_text = ""
+    if section[9] != "None" and section[9] != None:
+        citation_text += f"Viitteen tyyppi: {section[9],} "
     if section[0] != "None" and section[0] != None:
-        citation_text += f"Kirjoittaja: {section[0]}"
+        citation_text += f" Kirjoittaja: {section[0]}"
     if section[1] != "None" and section[1] != None:
         citation_text += f", Otsikko: {section[1]}"
     if section[2] != "None" and section[2] != None:
@@ -87,6 +89,7 @@ def add_section_to_citation(section):
         citation_text += f", Sivut: {section[7]}"
     if section[8] != "None" and section[8] != None:
         citation_text += f", Shorthand: {section[8]}"
+    
     return citation_text
 
 def get_one_citation(id):
