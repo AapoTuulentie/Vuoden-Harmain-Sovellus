6018c026bbc872f667723d11da9907f9e43181bf
Jaakko <jiisala@gmail.com>
2022-12-09 14:39:34 +0200

    bitex creator now owerwites file

--- src/bibtex_creator.py
+++ src/bibtex_creator.py
@@ -27,23 +27,26 @@ def create_bibtex_from_all_citations():
     username = session.get("user_name")
     
     all_citations = get_all_citations()
+    bibtex_string = ""
     try:
         for citation in all_citations.values():
-            bibtex_string = (f'@{citation["type"]}\u007b{citation["shorthand"]},\n')
+            bibtex_string += (f'@{citation["type"]}\u007b{citation["shorthand"]},\n')
             for key in citation.keys():
-                if key not in ["type", "shorthand"]:
+                if key not in ["type", "shorthand", "user_id"]:
                     if citation[key]:
                         bibtex_string +=(f'{key} = "{citation[key]}",\n')
             bibtex_string += "}\n"    
-            bibtex = open(f"{username}.bib", "a")
-            bibtex.write(bibtex_string)
-            bibtex.close()
+        bibtex = open(f"{username}.bib", "w")
+        bibtex.write(bibtex_string)
+        bibtex.close()
     except:
         return False        
     return True
 
 def create_bibtex_from_one_citation(id):
     all_citations = get_all_citations()
+    username = session.get("user_name")
+
     try:    
         citation = all_citations[id]
 
@@ -53,9 +56,11 @@ def create_bibtex_from_one_citation(id):
                 if citation[key]:
                     bibtex_string +=(f'{key} = "{citation[key]}",\n')
         bibtex_string += "}\n"    
-        bibtex = open(f'{citation["shorthand"]}.bib', "a")
+        bibtex = open(f'{username}_{citation["shorthand"]}.bib', "w")
         bibtex.write(bibtex_string)
         bibtex.close()
     except:
         return False        
     return True   
+
+()
